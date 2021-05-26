5881e88a65d5060b6ef3240d0706bf8b9d22e6d3
Yunsu <kenatman0723@gmail.com>
2021-05-26 10:00:00 +0900

    middleware pt.2(app.use() for global middlewares)

--- src/server.js
+++ src/server.js
@@ -4,16 +4,26 @@ const PORT = 4000;
 
 const app = express();
 
-const middleware = (req, res, next) => {
-  console.log(`Trying to reach : ${req.url}`);
+const logger = (req, res, next) => {
+  console.log(`${req.method} : ${req.url}`);
+  next();
+};
+
+const protecter = (req, res, next) => {
+  if (req.url === `/protected`) {
+    return res.send(`<h1>It is locked</h1>`);
+  }
+  console.log(`keep going...`);
   next();
 };
 
 const handleHome = (req, res) => {
   res.send(`handleHome`);
 };
-
-app.get("/", middleware, handleHome);
+app.use(logger);
+app.use(protecter);
+app.get("/", handleHome);
+app.get("/protected", (req, res) => res.send(`This is private lounge!!!`));
 
 const handleListening = () => {
   console.log(`✅ Server Listening on port http://localhost:${PORT} 🚀`);
