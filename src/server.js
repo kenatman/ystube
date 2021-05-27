8ed773aeb4f08b6d95137b1fd714f89f94a2c7cf
Yunsu <kenatman0723@gmail.com>
2021-05-27 09:52:38 +0900

    Concept of Router : mini-app for orgainizing URLs and Controllers

--- src/server.js
+++ src/server.js
@@ -5,15 +5,24 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {
-  res.send(`HOME`);
-};
-
 const logger = morgan("dev");
-
 app.use(logger);
-app.get("/", handleHome);
-app.get("/protected", (req, res) => res.send(`locked`));
+
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send(`HOME`);
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+const handleUserEdit = (req, res) => res.send(`USER-EDIT`);
+userRouter.get("/edit", handleUserEdit);
+
+const videoRouter = express.Router();
+const handleVideoWatch = (req, res) => res.send(`VIDEO-WATCH`);
+videoRouter.get("/watch", handleVideoWatch);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () => {
   console.log(`✅ Server Listening on port http://localhost:${PORT} 🚀`);
