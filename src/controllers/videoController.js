59b6d84b905974b36ef90e22c63a776d93690f23
Yunsu <kenatman0723@gmail.com>
2021-05-31 09:47:54 +0900

    Configuring PUG & partials : include

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,7 @@
-export const home = (req, res) => res.send(`HOME`);
+export const home = (req, res) => res.render("home");
+export const see = (req, res) => res.render("see");
+export const edit = (req, res) => res.render("edit");
+
 export const search = (req, res) => res.send(`SEARCH`);
-export const see = (req, res) => {
-  console.log(req.params.id);
-  res.send(`SEE VIDEO #${req.params.id}`);
-};
-export const edit = (req, res) => res.send(`EDIT VIDEO`);
 export const deleteVideo = (req, res) => res.send(`DELETE VIDEO`);
 export const upload = (req, res) => res.send(`UPLOAD VIDEO`);
