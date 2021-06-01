f1f96c083ba065f5a21a206920082ef9a422a3dc
Yunsu <kenatman0723@gmail.com>
2021-06-01 13:30:07 +0900

    PUG : absolute URL & relative URL

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,34 +1,38 @@
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "15 minutes ago",
+    views: 19,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 3,
+    comments: 2,
+    createdAt: "10 minutes ago",
+    views: 1,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 2,
+    comments: 2,
+    createdAt: "5 minutes ago",
+    views: 523,
+    id: 3,
+  },
+];
 export const home = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 3,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 2,
-    },
-    {
-      title: "Third Video",
-      rating: 2,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 3,
-    },
-  ];
   return res.render("home", { pageTitle: "HOME", videos });
 };
 
-export const see = (req, res) => res.render("see");
+export const see = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("see", { pageTitle: `Watching ${video.title}`, video });
+};
 export const edit = (req, res) => res.render("edit");
 
 export const search = (req, res) => res.send(`SEARCH`);
