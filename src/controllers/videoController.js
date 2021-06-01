f0a7df453b044be788c1c22a6fc40495c62d79fb
Yunsu <kenatman0723@gmail.com>
2021-06-01 10:27:39 +0900

    Mixin : smart partials : pre-made HTML block that receives data : include, +

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,30 @@
 export const home = (req, res) => {
-  const videos = [1, 2, 3];
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 3,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 2,
+    },
+    {
+      title: "Third Video",
+      rating: 2,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 3,
+    },
+  ];
   return res.render("home", { pageTitle: "HOME", videos });
 };
 
