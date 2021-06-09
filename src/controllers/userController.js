2c9c23eeebac7ccd24e8f3b4159d0fb09122659c
Yunsu <kenatman0723@gmail.com>
2021-06-09 13:00:08 +0900

    Join pt4. : form validation : (MongoDb operator)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,8 +3,39 @@ import User from "../models/User";
 export const getJoin = (req, res) => res.render("join", { pageTitle: `JOIN` });
 
 export const postJoin = async (req, res) => {
-  const { email, username, password, name, location } = req.body;
-  await User.create({ email, username, password, name, location });
+  const { email, username, password, password2, name, location } = req.body;
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle: `JOIN`,
+      errorMessage: `passwords don't match`,
+    });
+  }
+
+  const exist = await User.exists({ $or: [{ email }, { username }] });
+  if (exist) {
+    return res.render("join", {
+      pageTitle: `JOIN`,
+      errorMessage: `email/username already exists`,
+    });
+  }
+
+  // const emailExist = await User.exists({ email });
+  // if (emailExist) {
+  //   return res.render("join", {
+  //     pageTitle: `JOIN`,
+  //     errorMessage: `Email already exists`,
+  //   });
+  // }
+
+  // const usernameExist = await User.exists({ username });
+  // if (usernameExist) {
+  //   return res.render("join", {
+  //     pageTitle: `JOIN`,
+  //     errorMessage: `username already exists`,
+  //   });
+  // }
+
+  await User.create({ email, username, password, password2, name, location });
   return res.redirect("/login");
 };
 
