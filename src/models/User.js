cab3a3a2fa3c965edcecdffe21df4a5c608da735
Yunsu <kenatman0723@gmail.com>
2021-06-09 12:19:06 +0900

    Join pt3. : Hashing password

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userShcema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
@@ -6,6 +7,10 @@ const userShcema = new mongoose.Schema({
   password: { type: String, required: true },
   name: { type: String, required: true },
   location: String,
+});
+
+userShcema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 5);
 });
 
 const User = mongoose.model("User", userShcema);
