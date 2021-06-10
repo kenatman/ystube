73080b42a7231a0457014b20141a5a7078c10de1
Yunsu <kenatman0723@gmail.com>
2021-06-10 11:53:37 +0900

    Intro to Session : remembering activities between backend and browser.

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import session from "express-session";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
@@ -13,6 +14,16 @@ app.set("views", process.cwd() + "/src/views");
 
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(session({ secret: "Hello", resave: true, saveUninitialized: true }));
+
+// a middleware that shows sessions
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
 
 app.use("/", globalRouter);
 app.use("/users", userRouter);
