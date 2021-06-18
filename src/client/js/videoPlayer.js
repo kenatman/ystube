d7d1f8e28b46f548c431647bd06d724478e56118
Yunsu <kenatman0723@gmail.com>
2021-06-18 17:29:28 +0900

    Video Player : mute & volume

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,12 @@
 const playBtn = document.querySelector(`#play`);
 const muteBtn = document.querySelector(`#mute`);
 const time = document.querySelector(`#time`);
-const volume = document.querySelector(`#volume`);
+const volumeRange = document.querySelector(`#volume`);
 const video = document.querySelector("video");
+
+let volumeValue = 0.5;
+
+video.volume = volumeValue;
 
 const handlePlay = () => {
   if (video.paused) {
@@ -10,13 +14,31 @@ const handlePlay = () => {
   } else {
     video.pause();
   }
+  playBtn.innerText = video.paused ? `PLAY` : `PAUSE`;
 };
 
-const handleIconPlay = () => (playBtn.innerText = `PAUSE`);
-const handleIconPause = () => (playBtn.innerText = `PLAY`);
+const handleMute = () => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? `UNMUTE` : `MUTE`;
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumeRange = (e) => {
+  const {
+    target: { value },
+  } = e;
+  if (video.muted) {
+    muteBtn.innerText = `MUTE`;
+    video.muted = false;
+  }
+  volumeValue = value;
+  video.volume = value;
+};
 
 playBtn.addEventListener("click", handlePlay);
-video.addEventListener("play", handleIconPlay);
-video.addEventListener("pause", handleIconPause);
-
 muteBtn.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleVolumeRange);
