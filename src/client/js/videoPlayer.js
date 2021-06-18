858acafbaa83306d6c684bfd4717eecf77db3315
Yunsu <kenatman0723@gmail.com>
2021-06-18 16:17:35 +0900

    Video Player : play & pause

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,22 @@
-console.log(`video player`);
+const playBtn = document.querySelector(`#play`);
+const muteBtn = document.querySelector(`#mute`);
+const time = document.querySelector(`#time`);
+const volume = document.querySelector(`#volume`);
+const video = document.querySelector("video");
+
+const handlePlay = () => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+const handleIconPlay = () => (playBtn.innerText = `PAUSE`);
+const handleIconPause = () => (playBtn.innerText = `PLAY`);
+
+playBtn.addEventListener("click", handlePlay);
+video.addEventListener("play", handleIconPlay);
+video.addEventListener("pause", handleIconPause);
+
+muteBtn.addEventListener("click", handleMute);
