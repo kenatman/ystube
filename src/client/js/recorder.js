f5c3da2a36d1b78baef52c6dcecd656b8d1a90ca
Yunsu <kenatman0723@gmail.com>
2021-06-22 14:20:01 +0900

    Recorder : preview : video.srcObject()

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,13 @@
 const recorderBtn = document.getElementById("recorderBtn");
-
+const video = document.getElementById("preview");
+console.log(video);
 const handleStart = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true,
   });
-  console.log(stream);
+  video.srcObject = stream;
+  video.play();
 };
 
 recorderBtn.addEventListener("click", handleStart);
