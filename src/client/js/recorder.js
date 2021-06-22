c8cb30993c2a87289839ed36f40ef8248b777f48
Yunsu <kenatman0723@gmail.com>
2021-06-22 15:46:10 +0900

    Recorder : URL.createObjectURL() : create URL available only on the browser's memory

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -2,25 +2,30 @@ const recorderBtn = document.getElementById("recorderBtn");
 const video = document.getElementById("preview");
 
 let stream;
+let recorder;
+
+const handleDownload = () => {};
 
 const handleStop = () => {
-  recorderBtn.innerText = `Start Recording!!`;
+  recorderBtn.innerText = `Download Recording~~`;
   recorderBtn.removeEventListener("click", handleStop);
-  recorderBtn.addEventListener("click", handleStart);
+  recorderBtn.addEventListener("click", handleDownload);
+  recorder.stop();
+  recorder.ondataavailable = (e) => {
+    const videoFile = URL.createObjectURL(e.data); // create URL available only on the browser's memory
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
 };
 
 const handleStart = () => {
-  recorderBtn.innerText = `Stop Recording...`;
+  recorderBtn.innerText = `Stop Recording`;
   recorderBtn.removeEventListener("click", handleStart);
   recorderBtn.addEventListener("click", handleStop);
-  const recorder = new MediaRecorder(stream);
+  recorder = new MediaRecorder(stream);
   recorder.start();
-  setTimeout(() => {
-    recorder.stop();
-  }, 3000);
-  recorder.ondataavailable = (e) => {
-    console.log(e.data);
-  };
 };
 
 const init = async () => {
