d2c046418f849f7cd5e3f41705b62c5da6372280
Yunsu <kenatman0723@gmail.com>
2021-06-22 14:34:52 +0900

    Recorder : using MediaRecorder web API

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,37 @@
 const recorderBtn = document.getElementById("recorderBtn");
 const video = document.getElementById("preview");
-console.log(video);
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+
+let stream;
+
+const handleStop = () => {
+  recorderBtn.innerText = `Start Recording!!`;
+  recorderBtn.removeEventListener("click", handleStop);
+  recorderBtn.addEventListener("click", handleStart);
+};
+
+const handleStart = () => {
+  recorderBtn.innerText = `Stop Recording...`;
+  recorderBtn.removeEventListener("click", handleStart);
+  recorderBtn.addEventListener("click", handleStop);
+  const recorder = new MediaRecorder(stream);
+  recorder.start();
+  setTimeout(() => {
+    recorder.stop();
+  }, 3000);
+  recorder.ondataavailable = (e) => {
+    console.log(e.data);
+  };
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true,
   });
   video.srcObject = stream;
   video.play();
 };
+
+init();
 
 recorderBtn.addEventListener("click", handleStart);
