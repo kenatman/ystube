a29ec9a734bcb2f5dd478848623b04ca4b6d62dd
Yunsu <kenatman0723@gmail.com>
2021-06-23 15:48:00 +0900

    Converting video format pt3 : more FFmpeg options : get thumbnail

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -12,18 +12,37 @@ const handleDownload = async () => {
   await ffmpeg.load();
   ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
   await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+  await ffmpeg.run(
+    "-i",
+    "recording.webm",
+    "-ss",
+    "00:00:01",
+    "-frames:v",
+    "1",
+    "thumbnail.jpg"
+  );
 
   const mp4File = ffmpeg.FS("readFile", "output.mp4");
-  console.log(mp4File);
-  console.log(mp4File.buffer);
+  const thumbnailFile = ffmpeg.FS("readFile", "thumbnail.jpg");
+  // Until here... ffmpeg part. not JS..
+
   const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+  const thumbnailBlob = new Blob([thumbnailFile.buffer], { type: "image/jpg" });
+
   const mp4Url = URL.createObjectURL(mp4Blob);
+  const thumbnailUrl = URL.createObjectURL(thumbnailBlob);
 
   const a = document.createElement("a");
   a.href = mp4Url;
   a.download = "My Video.mp4";
   document.body.appendChild(a);
   a.click();
+
+  const thumbA = document.createElement("a");
+  thumbA.href = thumbnailUrl;
+  thumbA.download = "My thumbnail.jpg";
+  document.body.appendChild(thumbA);
+  thumbA.click();
 };
 
 const handleStop = () => {
