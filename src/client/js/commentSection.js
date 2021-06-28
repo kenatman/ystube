7a4c338062fae9121a68f5031a58399dfbfef0e7
Yunsu <kenatman0723@gmail.com>
2021-06-28 11:07:08 +0900

    Comment pt6. : Rendering realtime comments : making HTML from frontend JS

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,6 +1,19 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
+const addComments = (text) => {
+  const commentsUl = document.querySelector(`.video__comments ul`);
+  const li = document.createElement("li");
+  li.className = `video__comment`;
+  const i = document.createElement("i");
+  i.className = `fas fa-comment`;
+  const span = document.createElement("span");
+  span.innerText = text;
+  li.appendChild(i);
+  li.appendChild(span);
+  commentsUl.prepend(li);
+};
+
 const handleSubmit = async (e) => {
   e.preventDefault();
   const textarea = form.querySelector(`textarea`);
@@ -9,14 +22,16 @@ const handleSubmit = async (e) => {
   if (text === ``) {
     return;
   }
-  await fetch(`/api/videos/${videoId}/comment`, {
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     //headers : information about request
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ text }),
   });
   textarea.value = "";
-  window.location.reload();
+  if (response.status === 201) {
+    addComments(text);
+  }
 };
 
 if (form) {
