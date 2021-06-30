0804bee0d7fa8b4aacfc5f3f974db6a8c7069adf
Yunsu <kenatman0723@gmail.com>
2021-07-01 08:55:57 +0900

    Deployment : heroku PORT variable

--- src/init.js
+++ src/init.js
@@ -6,7 +6,7 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
-const PORT = 4500;
+const PORT = process.env.PORT || 4500;
 
 const handleListening = () => {
   console.log(`✅ Server Listening on port http://localhost:${PORT} 🚀`);
